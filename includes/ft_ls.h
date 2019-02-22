7084bcfbe042ec8ec5ddb8136dfbf639478d9339
madvil2 <madvil2@mail.ru>
2019-02-22 22:41:48 +0300

    goals

---
+++ includes/ft_ls.h
@@ -0,0 +1,26 @@
+#ifndef _FT_LS_H_
+#define _FT_LS_H_
+
+# include <stdio.h>
+# include <dirent.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <fcntl.h>
+# include <time.h>
+# include "../libft/libft.h"
+# include <grp.h>
+
+struct s_ls {
+	int l;
+	int r;
+	int l_r;
+	int a;
+	int	t;
+
+
+	
+
+}		t_ls;
+
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include "../includes/ft_ls.h"
+
+int main()
+{
+/* 	DIR *a;
+	struct dirent *entry;
+
+	a = opendir("/Users/pcollio-/Projects/ft_ls/test");
+
+	while ((entry = readdir(a)))
+	{
+		ft_printf("%lld - %s [%d] %d %d %lld\n",
+			   entry->d_ino, entry->d_name, entry->d_type, entry->d_reclen, entry->d_namlen, entry->d_seekoff);
+	}
+ */
+
+	struct stat s;
+
+	stat("/Users/pcollio-/Projects/ft_ls/test", &s);
+	/* printf( (S_ISDIR(s.st_mode)) ? "d" : "-");
+    printf( (s.st_mode & S_IRUSR) ? "r" : "-");
+    printf( (s.st_mode & S_IWUSR) ? "w" : "-");
+    printf( (s.st_mode & S_IXUSR) ? "x" : "-");
+    printf( (s.st_mode & S_IRGRP) ? "r" : "-");
+    printf( (s.st_mode & S_IWGRP) ? "w" : "-");
+    printf( (s.st_mode & S_IXGRP) ? "x" : "-");
+    printf( (s.st_mode & S_IROTH) ? "r" : "-");
+    printf( (s.st_mode & S_IWOTH) ? "w" : "-");
+    printf( (s.st_mode & S_IXOTH) ? "x" : "-"); */
+	/* time_t lt;
+	lt = time(NULL);
+	printf("%s", ctime(&lt)); */
+
+	system("tput cols");
+
+	ft_printf("ф");
+
+	return 0;
+}
