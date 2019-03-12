59e9fa1600e6575b28cb55a1a6d467a63d851eff
Dremora Restless draugr <drestles@e2r10p1.21-school.ru>
2019-03-13 00:19:04 +0300

    minor major

--- src/l_format.c
+++ src/l_format.c
@@ -6,7 +6,7 @@
 /*   By: drestles <drestles@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/10 12:25:18 by drestles          #+#    #+#             */
-/*   Updated: 2019/03/12 23:15:03 by pcollio-         ###   ########.fr       */
+/*   Updated: 2019/03/13 00:18:27 by drestles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,15 @@ static void	l_format_print_dev(char *file, t_format *format)
 	format->group = get_group(file);
 	format->date = get_last_time(file);
 	format->name = put_link(file);
-	printf("%s %*d %-*s  %-*s  %*d, %*d %s %s\n", format->chmod,
+	if (obj_type(file) == 2 || obj_type(file) == 3)
+		printf("%s %*d %-*s  %-*s  %*d, %*d %s %s\n", format->chmod,
 	format->max0, vtorya_hernya(file), format->max1, format->user,
 	format->max2, format->group, format->max3, get_major(file),
 	format->max4, get_minor(file), format->date, format->name);
+	else
+		ft_printf("%s %*d %-*s  %-*s  %*s %*lld %s %s\n", format->chmod, format->max0,
+	vtorya_hernya(file), format->max1, format->user, format->max2,
+	format->group, format->max4, "     ", format->max5, get_size(file), format->date, format->name);
 	free(format->date);
 	free(format->chmod);
 	free(format->name);
